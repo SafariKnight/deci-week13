f2e3dfc7dddae50703226256f7bcf003135955f8
Kareem Hisham <safariknight2@gmail.com>
2026-07-07 15:49:29 +0300

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
+import "./App.css"
+
 export default function App() {
-  return <>
-  <p>
-    Hello, World!
-  </p>
-  Team 2:
+  return <main>
+    <h2>Team 2</h2>
     <ul>
       <li>Kareem Hisham</li>
       <li>Mohammed Mahmoud</li>
       <li>Younes Mohammed</li>
-  </ul>
-  </>
+    </ul>
+  </main>
 }
